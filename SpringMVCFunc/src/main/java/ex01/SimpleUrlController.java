64c851989354ec55a9737c640717e24478659492
Yooninseoung <isyoon2000@naver.com>
2022-04-02 17:39:30 +0900

    MultiActionController

--- SpringMVCFunc/src/main/java/ex01/SimpleUrlController.java
+++ SpringMVCFunc/src/main/java/ex01/SimpleUrlController.java
@@ -9,6 +9,6 @@ import org.springframework.web.servlet.mvc.Controller;
 public class SimpleUrlController implements Controller {
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return new ModelAndView("index.jsp"); //작업을 마친 후 뷰이름을 ModelAndView에 index.jsp로 설정하여 반환한다.
+		return new ModelAndView("index.jsp");
 	}
 }
