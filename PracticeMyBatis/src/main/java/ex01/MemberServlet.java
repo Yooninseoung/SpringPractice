1462ec157f4b447c1110a6871dc77d5dcf3bd08f
Yooninseoung <isyoon2000@naver.com>
2022-04-10 13:25:20 +0900

    mybatis practice..

---
+++ PracticeMyBatis/src/main/java/ex01/MemberServlet.java
@@ -0,0 +1,54 @@
+package ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/mem.do")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MemberServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+		
+	}
+	
+	
+protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> membersList = dao.selectAllMemberList();
+		request.setAttribute("memberList", membersList);
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("test/listMembers.jsp");
+		dispatch.forward(request, response);
+	}
+
+}
