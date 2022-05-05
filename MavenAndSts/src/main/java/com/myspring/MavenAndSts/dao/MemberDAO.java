84a3e0c017c02271272cb22e9141b2ce7d953cb1
Yooninseoung <isyoon2000@naver.com>
2022-05-05 15:31:01 +0900

    MavenAndSts 1

---
+++ MavenAndSts/src/main/java/com/myspring/MavenAndSts/dao/MemberDAO.java
@@ -0,0 +1,16 @@
+package com.myspring.MavenAndSts.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.myspring.MavenAndSts.vo.MemberVO;
+
+
+public interface MemberDAO {
+	 public List selectAllMemberList() throws DataAccessException;
+	 public int insertMember(MemberVO memberVO) throws DataAccessException ;
+	 public int deleteMember(String id) throws DataAccessException;
+	 
+
+}
---
+++ MavenAndSts/src/main/java/com/myspring/MavenAndSts/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.myspring.MavenAndSts.service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import com.myspring.MavenAndSts.vo.MemberVO;
+
+public interface MemberService {
+	 public List listMembers() throws DataAccessException;
+	 public int addMember(MemberVO memberVO) throws DataAccessException;
+	 public int removeMember(String id) throws DataAccessException;
+
+}
