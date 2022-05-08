50bbf708017dfbf92dca45e21a3a7550740b3b4b
Yooninseoung <isyoon2000@naver.com>
2022-05-08 22:43:30 +0900

    log4jAndTiles

--- MavenAndSts/src/main/java/com/myspring/MavenAndSts/member/controller/MemberController.java
+++ MavenAndSts/src/main/java/com/myspring/MavenAndSts/member/controller/MemberController.java
@@ -3,10 +3,10 @@ package com.myspring.MavenAndSts.member.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myspring.MavenAndSts.vo.MemberVO;
 
@@ -14,5 +14,6 @@ public interface MemberController {
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView addMember(@ModelAttribute("info") MemberVO memberVO,HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView removeMember(@RequestParam("id") String id, HttpServletRequest request, HttpServletResponse response) throws Exception;
-
+	public ModelAndView login(@ModelAttribute("member") MemberVO memberVO,RedirectAttributes rAttr,HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
--- MavenAndSts/src/main/java/com/myspring/MavenAndSts/member/controller/MemberControllerImpl.java
+++ MavenAndSts/src/main/java/com/myspring/MavenAndSts/member/controller/MemberControllerImpl.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myspring.MavenAndSts.service.MemberService;
 import com.myspring.MavenAndSts.vo.MemberVO;
@@ -21,6 +25,8 @@ import com.myspring.MavenAndSts.vo.MemberVO;
 
 @Controller("memberController")
 public class MemberControllerImpl   implements MemberController {
+	private static final Logger logger = LoggerFactory.getLogger(MemberControllerImpl.class);
+	
 	@Autowired
 	private MemberService memberService;
 	@Autowired
@@ -30,6 +36,8 @@ public class MemberControllerImpl   implements MemberController {
 	@RequestMapping(value="/member/listMembers.do" ,method = RequestMethod.GET)
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String viewName = getViewName(request);
+		logger.info("info전용 viewName = " + viewName); //메소드에 따라 레벨이 다름
+		logger.debug("debug전용 viewName = " + viewName);
 		List membersList = memberService.listMembers();
 		ModelAndView mav = new ModelAndView(viewName);
 		mav.addObject("membersList", membersList);
@@ -57,14 +65,48 @@ public class MemberControllerImpl   implements MemberController {
 		return mav;
 	}
 	
-	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
-	public ModelAndView form(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String viewName = getViewName(request);
+	
+	@Override
+	@RequestMapping(value = "/member/login.do", method = RequestMethod.POST)
+	public ModelAndView login(@ModelAttribute("member") MemberVO member,
+				              RedirectAttributes rAttr,
+		                       HttpServletRequest request, HttpServletResponse response) throws Exception {
+	ModelAndView mav = new ModelAndView();
+	memberVO = memberService.login(member);
+	if(memberVO != null) {
+		    HttpSession session = request.getSession();
+		    session.setAttribute("member", memberVO);
+		    session.setAttribute("isLogOn", true);
+		    mav.setViewName("redirect:/member/listMembers.do");
+	}else {
+		    rAttr.addAttribute("result","loginFailed");
+		    mav.setViewName("redirect:/member/loginForm.do");
+	}
+	return mav;
+	}
+
+	@Override
+	@RequestMapping(value = "/member/logout.do", method =  RequestMethod.GET)
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		session.removeAttribute("member");
+		session.removeAttribute("isLogOn");
 		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/member/listMembers.do");
+		return mav;
+	}	
+
+	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
+	private ModelAndView form(@RequestParam(value= "result", required=false) String result,
+						       HttpServletRequest request, 
+						       HttpServletResponse response) throws Exception {
+		
+		String viewName = (String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("result",result);
 		mav.setViewName(viewName);
 		return mav;
 	}
-	
 	
 
 	private String getViewName(HttpServletRequest request) throws Exception {
